4baa2b87275462ebc3a4c6ddd87d93ff4a40b3a6
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2025-01-26 00:00:00 +0530

    Created created application configs

---
+++ src/configs/config.ts
@@ -0,0 +1,5 @@
+export const animationDuration = 100;
+export const defaultBoardSize = 4;
+export const supportedBoardSizes = [3, 4, 5, 6];
+export const victoryTileValue = 2048;
+export const gridGap = '1rem';
