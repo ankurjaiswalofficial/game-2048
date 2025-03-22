e55abc43ddf02700f46656be99e4b3d14fc0f06f
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2025-03-23 02:43:50 +0530

    added my own logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/logo.png" type="image/png" />
+            </head>
             <body className={`${openSans.variable} antialiased size-full pb-4`}>
                 <ThemeProvider>
                     <StoreProvider>{children}</StoreProvider>
