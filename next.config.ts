f7dcf78a9e458b0238f912b1fe57f7d6b6a1897b
Ankur Jaiswal <ankurjaisawalofficial@gmail.com>
2025-12-26 13:46:57 +0530

    updated basePath to use STATIC_URL_PATH

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.STATIC_URL_PATH || "",
 };
 
 export default nextConfig;
