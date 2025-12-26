537a21d7110d4b34aee17c07f1ed3127e549eca3
Ankur Jaiswal <ankurjaisawalofficial@gmail.com>
2025-12-26 14:23:17 +0530

    reverted changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.STATIC_URL_PATH || "",
+  /* config options here */
 };
 
 export default nextConfig;
